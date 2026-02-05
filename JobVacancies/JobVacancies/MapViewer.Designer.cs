8fbeffe129fefec18f824453884761d9626a643b
WarLott <Lotte.Meier93@gmail.com>
2026-02-06 00:32:32 +0100

    Adds movable image (map is too big still).

---
+++ JobVacancies/JobVacancies/MapViewer.Designer.cs
@@ -0,0 +1,83 @@
+﻿using System.Windows.Forms;
+
+namespace JobVacancies
+{
+    partial class MapViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MapViewer));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1854, 828);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_OnMouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_OnMouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_OnMouseUp);
+            // 
+            // MapViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1855, 829);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "MapViewer";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MapViewer_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ JobVacancies/JobVacancies/MapViewer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JobVacancies
+{
+    public partial class MapViewer : Form
+    {
+        private bool inPan = false;
+        private Point panStart = Point.Empty;
+        private Point imagePos = Point.Empty;
+        private Image Image;
+
+        public MapViewer()
+        {
+            InitializeComponent();
+            Image = pictureBox1.Image;
+        }
+
+        private void MapViewer_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Focus();
+                inPan = true;
+                panStart = new Point(e.Location.X - imagePos.X,
+                                     e.Location.Y - imagePos.Y);
+            }
+        }
+
+        private void pictureBox1_OnMouseUp(object sender, MouseEventArgs e)
+        {
+            inPan = false;
+        }
+
+        private void pictureBox1_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            inPan = false;
+        }
+
+        private void pictureBox1_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (inPan)
+            {
+                imagePos = new Point(e.Location.X - panStart.X,
+                                     e.Location.Y - panStart.Y);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.Clear(Color.Blue);
+            var img = Image.FromFile(@"C:\Users\Lotte\Pictures\03-3D\download_20170803_142808.jpg");
+            e.Graphics.DrawImage(img, imagePos);
+            img.Dispose();
+            e.Dispose();
+            Debug.WriteLine($"new pos {imagePos}");
+        }
+    }
+}
